bcfcdb237c8dd5341cd924091ca11f730a1470b5
Arthur Correnson <arthur.correnson@gmail.com>
2019-08-30 09:01:46 +0200

    update ast processor
    + output valid python code
    + type annotations
    
    Todos :
    + CLI
    + dynamic check

--- src/ast_processor.ml
+++ src/ast_processor.ml
@@ -7,8 +7,56 @@ open Printf
 open Ast
 
 (* =============================================== *)
+(* PYTHON GENERATION UTILS                         *)
+(* =============================================== *)
+
+(** Create a i long empty string *)
+let tab i = (String.make i ' ')
+
+
+(** Output a python class definition header
+  @param extends Name of the parent class
+*)
+let pclass ?extends:(parent="") name =
+  match parent with
+  | ""  -> printf "class %s:\n" name
+  | parent_name -> printf "class %s(%s):\n" name parent_name
+
+
+(** Output a python __init__ method  
+  @param product List of the types associated to each field
+*)
+let init product =
+  let rec step_fields remaining_fields counter =
+    match remaining_fields with
+    | []  -> ()
+    | typename::rest ->
+      printf "%sfield_%d : %s\n" (tab 4) counter typename;
+      step_fields rest (counter+1)
+  in
+
+  let arg_n   i t = printf ", arg_%d : %s " i t in
+  let field_n i _ = printf "%sfield_%d = arg_%d\n" (tab 8) i i in
+  
+  (* write TYPE informations  *)
+  step_fields product 0;
+  (* write class constructor  *)
+  printf "%sdef __init__(self" (tab 4);
+  
+  List.iteri arg_n    product;
+  printf ")\n";
+  List.iteri field_n  product;
+  
+  (* empty constructor *)
+  if List.length product = 0 then printf "%s pass\n" (tab 8)
+
+
+(* =============================================== *)
 (* SUBCLASSES GENERATION - OCAML-LIKE CONSTRUCTORS *)
 (* =============================================== *)
+
+
+(** Output python subclasses (ocaml constructors)  *)
 let rec write_sub_classes parent constructors =
   match constructors with
   | [] -> ()
@@ -19,31 +67,30 @@ let rec write_sub_classes parent constructors =
   | _ -> failwith "ERROR - SUBCLASSES ARE CREATED ONLY FROM CUSTOM CONSTRUCTORS"
 
 and write_sub_class parent name product =
-  printf "class %s(%s):\n    def __init__(self):\n        pass\n" name parent; ignore(product)
+  pclass ~extends:parent name; init product
 
 
 (* =============================================== *)
 (* MAIN CLASS GENERATION - TYPE WRAPPER            *)
 (* =============================================== *)
+
+
+(** Output python main class (ocaml type) *)
 let write_main_class name =
-  printf "class %s :\n    def __init__(self):\n        pass\n" name
-
-
-(* =============================================== *)
-(* OUTPUT PYTHON CLASSES                           *)
-(* =============================================== *)
-let write_class name constructors =
-  write_main_class name;
-  print_newline ();
-  write_sub_classes name constructors
+  pclass name; init []
 
 
 (* =============================================== *)
 (* TYPEDEF TO PYTHON CONVERSION                    *)
 (* =============================================== *)
-let make_class typedef =
+
+
+(** Convert typedef to python code *)
+let write_classes typedef =
   match typedef with
-  | Typedef (name, constructors)  ->
-    write_class name constructors
+  | Typedef (name, constructors) ->
+    write_main_class name;
+    print_newline ();
+    write_sub_classes name constructors
   | _ ->
     failwith "ERROR - CLASS ARE CREATED ONLY FROM TYPEDEF"
