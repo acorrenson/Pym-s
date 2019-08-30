16fcc97bc6431f19eace7426c88ca1ac1de3a973
Arthur Correnson <arthur.correnson@gmail.com>
2019-08-30 10:58:06 +0200

    add minimal CLI

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ build:
 	@ cd src ; dune build main.exe
 
 run:
-	@ ./src/_build/default/main.exe
+	@ ./src/_build/default/main.exe test.pyms
 
 clean:
 	@ rm -rf ./src/*.cmi ./src/*.cmo
--- src/main.ml
+++ src/main.ml
@@ -5,13 +5,23 @@
 
 open Ast_processor
 open Printf
+open Sys
+open Filename
 
+
+(* =============================================== *)
+(* PARSING UTILS                                   *)
+(* =============================================== *)
+
+
+(** Print the current position of the lexer *)
 let print_position outx lexbuf =
   let pos = lexbuf.Lexing.lex_curr_p in
   fprintf outx "%s:%d:%d" pos.pos_fname
     pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
 
 
+(** Parse with error/debug informations *)
 let parse_with_error lexbuf =
   try 
     let ast = Parser.main Lexer.token lexbuf in
@@ -25,7 +35,41 @@ let parse_with_error lexbuf =
       exit 1
 
 
-let _ =
-  let lexbuf = Lexing.from_channel (open_in "test.pyms") in
-  lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = "test.pyms" };
+(** resolve relative path *)
+let resolve fname = 
+  if is_relative fname then
+      concat (getcwd ()) fname
+  else fname
+
+
+(** Process the input file *)
+let process_file fname =
+  printf "Pyms-s running on [ %s ]\n\n" fname;
+  
+  let lexbuf = Lexing.from_channel (open_in fname) in
+    
+  lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with
+    pos_fname = (basename fname) };
+  
   parse_with_error lexbuf
+
+
+(* process the input file with error/debug informations *)
+let process_file_with_error fname = 
+  let full_name = resolve fname in
+  if file_exists full_name then begin 
+    if check_suffix full_name "pyms" then
+      process_file full_name
+    else failwith "[ ERROR - input files should have .pyms extension\n" end
+  else
+    fprintf stderr "[ INPUT ERROR - file %s does'nt exist ]\n" fname;
+    exit 1
+
+
+(* =============================================== *)
+(* COMMAND LINE INTERFACE SETUP                    *)
+(* =============================================== *)
+
+let _ =
+  let speclist = [] in
+  Arg.parse speclist (process_file) "pyms file.pyms [options]"
